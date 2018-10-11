046044b201f8f6932600f8b678b2733b2601a7f7
Alex <razielrazi3@gmail.com>
2018-10-11 23:46:54 +0300

    add treble makefile
    for some reason this was missing

---
+++ treble_arm64_avN.mk
@@ -0,0 +1,51 @@
+$(call inherit-product, device/phh/treble/base-pre.mk)
+include build/make/target/product/treble_common.mk
+$(call inherit-product, vendor/vndk/vndk.mk)
+$(call inherit-product, device/phh/treble/base.mk)
+$(call inherit-product, device/phh/treble/pixel.mk)
+
+PRODUCT_NAME := treble_arm64_avN
+PRODUCT_DEVICE := phhgsi_arm64_a
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := Phh-Treble vanilla
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=8m \
+    ro.sys.fw.dex2oat_thread_count=4 \
+    dalvik.vm.dex2oat-filter=speed \
+    dalvik.vm.image-dex2oat-filter=speed
+
+# HWUI
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.texture_cache_size=128 \
+    ro.hwui.layer_cache_size=64 \
+    ro.hwui.r_buffer_cache_size=64 \
+    ro.hwui.path_cache_size=64 \
+    ro.hwui.gradient_cache_size=5 \
+    ro.hwui.drop_shadow_cache_size=8 \
+    ro.hwui.texture_cache_flushrate=0.4 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=1024 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=1024
+
+# Graphics
+TARGET_USES_HWC2 := true
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+OVERRIDE_RS_DRIVER := libRSDriverArm.so
+
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := 560dpi
+PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
+
