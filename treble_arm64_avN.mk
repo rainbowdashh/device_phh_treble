85eaa542b29146167295bad10d3993e7d59bbe59
Alex <razielrazi3@gmail.com>
2018-10-12 19:37:48 +0300

    update treble makefile

--- treble_arm64_avN.mk
+++ treble_arm64_avN.mk
@@ -2,12 +2,13 @@ $(call inherit-product, device/phh/treble/base-pre.mk)
 include build/make/target/product/treble_common.mk
 $(call inherit-product, vendor/vndk/vndk.mk)
 $(call inherit-product, device/phh/treble/base.mk)
+
 $(call inherit-product, device/phh/treble/pixel.mk)
 
 PRODUCT_NAME := treble_arm64_avN
 PRODUCT_DEVICE := phhgsi_arm64_a
 PRODUCT_BRAND := Android
-PRODUCT_MODEL := Phh-Treble vanilla
+PRODUCT_MODEL := Alex S9 semi-gsi
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2560
