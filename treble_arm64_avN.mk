35826e704f57b9b3d59881b79eab69222a1d4def
Alex <razielrazi3@gmail.com>
2018-12-08 21:34:26 +0200

    havocos bringup

--- treble_arm64_avN.mk
+++ treble_arm64_avN.mk
@@ -2,8 +2,7 @@ $(call inherit-product, device/phh/treble/base-pre.mk)
 include build/make/target/product/treble_common.mk
 $(call inherit-product, vendor/vndk/vndk.mk)
 $(call inherit-product, device/phh/treble/base.mk)
-
-$(call inherit-product, device/phh/treble/pixel.mk)
+$(call inherit-product, device/phh/treble/havoc.mk)
 
 PRODUCT_NAME := treble_arm64_avN
 PRODUCT_DEVICE := phhgsi_arm64_a
